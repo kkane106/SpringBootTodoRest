5feb92d93bc8c0aa6ae86cd551f1589ef040cb2e
Kris Kane <kkane106@gmail.com>
2016-08-03 14:39:06 -0600

    Rename 'HelloCtrl' to 'WelcomeCtrl' for clarity

--- src/main/java/demo/WelcomeCtrl.java
+++ src/main/java/demo/WelcomeCtrl.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloCtrl {
+public class WelcomeCtrl {
 	@RequestMapping("/")
-	public String hello() {
+	public String index() {
 		return "index.html";
 	}
 	
