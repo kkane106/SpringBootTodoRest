312201ecff40089ae5fdb1e68e378e09907904d9
Kris Kane <kkane106@gmail.com>
2016-08-03 14:34:23 -0600

    Add a static view (index.html), Add Angular, Test $http service with a custom factory

--- src/main/java/demo/HelloCtrl.java
+++ src/main/java/demo/HelloCtrl.java
@@ -1,18 +1,13 @@
 package demo;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloCtrl {
-	@RequestMapping(value="/hello")
+	@RequestMapping("/")
 	public String hello() {
-		return "Hello World!";
+		return "index.html";
 	}
 	
 }
